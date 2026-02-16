23da718b0a763b1ffc53cebf78fd264c5aec0502
andre-maestas <andre.maestas@unity3d.com>
2026-02-16 14:12:44 -0800

    init commit

---
+++ Models/Project.cs
@@ -0,0 +1,13 @@
+namespace PortfolioWebsiteProject.Models
+{
+    public class Project
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string ImageSrc { get; set; } = "images/error.svg";
+        public string ButtonText { get; set; } = string.Empty;
+        public string ButtonUrl { get; set; } = string.Empty;
+        public string[] Techs { get; set; } = System.Array.Empty<string>();
+        public string Type { get; set; } = string.Empty;
+    }
+}
