55efed810fdb78e822763c16d961a3e390b620f8
Andre Maestas <andremaestas13@gmail.com>
2026-03-08 10:53:48 -0700

    Refactor project to Blazor WebAssembly
    - Converted Program.cs to use WebAssemblyHostBuilder for Blazor WebAssembly setup.
    - Removed launchSettings.json, appsettings.json, and appsettings.Development.json as they are not needed for the Blazor WebAssembly project.
    - Deleted design.jsonc as it is no longer relevant.
    - Added ProjectView.razor and associated styles for project card display.
    - Created projects.json to store project data for the portfolio.
    - Added new images for projects to the wwwroot/images directory.
    - Created index.html for the Blazor WebAssembly app entry point.
    - Updated _Imports.razor to include necessary namespaces for components.

--- Models/Project.cs
+++ Models/Project.cs
@@ -1,4 +1,4 @@
-namespace PortfolioWebsiteProject.Models
+namespace PortfolioSiteWasm.Models
 {
     public class Project
     {
@@ -6,9 +6,11 @@ namespace PortfolioWebsiteProject.Models
         public string Description { get; set; } = string.Empty;
         public string LongDescription { get; set; } = string.Empty;
         public string ImageSrc { get; set; } = "images/error.svg";
+
+        public string YoutubeURL { get; set; } = string.Empty;
         public string URL { get; set; } = string.Empty;
-        public string[] Techs { get; set; } = System.Array.Empty<string>();
-        public string[] Platforms { get; set; } = System.Array.Empty<string>();
+        public string[] Techs { get; set; } = Array.Empty<string>();
+        public string[] Platforms { get; set; } = Array.Empty<string>();
         public string Type { get; set; } = string.Empty;
         public int Year { get; set; } = DateTime.MinValue.Year;
     }
