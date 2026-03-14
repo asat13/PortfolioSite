2dec4965d15c396dac0cadde32b3323e1ad53d03
Andre Maestas <andremaestas13@gmail.com>
2026-03-14 10:09:57 -0700

    Add project category and contribution fields; update project details and images

--- Models/Project.cs
+++ Models/Project.cs
@@ -13,5 +13,7 @@ namespace PortfolioSiteWasm.Models
         public string[] Platforms { get; set; } = Array.Empty<string>();
         public string Type { get; set; } = string.Empty;
         public int Year { get; set; } = DateTime.MinValue.Year;
+        public string Contribution { get; set; } = string.Empty;
+        public string ProjectCategory { get; set; } = string.Empty;
     }
 }
